4fad03960e4612ced9221fff5b2637e828278268
benjabarreiro <barreirobenjamin@gmail.com>
2024-06-05 08:15:09 -0300

    fix: validate req body and simplify logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,34 +1,64 @@
 import {
+  ArgumentMetadata,
+  BadRequestException,
   Body,
   Controller,
   HttpException,
   HttpStatus,
+  Injectable,
+  PipeTransform,
   Post,
+  UsePipes,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateUserDto, LoginUserDto } from './dtos';
+import { CreateUserDto, LoginUserDto, UserDto } from './dtos';
+import * as Joi from 'joi';
+
+const userSchema = Joi.object({
+  userName: Joi.string()
+    .pattern(/^[a-zA-Z]+$/)
+    .exist()
+    .min(2),
+  password: Joi.string().exist().min(4),
+});
+
+@Injectable()
+export class JoiValidationPipe implements PipeTransform {
+  constructor(private schema: Joi.ObjectSchema) {}
+
+  transform(value: UserDto) {
+    const { error } = this.schema.validate(value, { abortEarly: false });
+    if (error) {
+      const buildErrorMessage = {};
+      error.details.map(
+        (err) =>
+          (buildErrorMessage[err.context.label] = err.message.replace(
+            /[\\"]/g,
+            '',
+          )),
+      );
+      throw new BadRequestException({
+        input: error._original,
+        errors: buildErrorMessage,
+      });
+    }
+    return value;
+  }
+}
 
 @Controller('/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/register')
+  @UsePipes(new JoiValidationPipe(userSchema))
   createUser(@Body() body: CreateUserDto): Promise<string> | string {
-    if (!body || !Object.keys(body).length)
-      throw new HttpException(
-        'Body cannot be empty' + body,
-        HttpStatus.BAD_REQUEST,
-      );
     return this.authService.createUser(body);
   }
 
   @Post('/login')
+  @UsePipes(new JoiValidationPipe(userSchema))
   login(@Body() body: LoginUserDto): Promise<string> | string {
-    if (!body || !Object.keys(body).length)
-      throw new HttpException(
-        'Body cannot be empty' + body,
-        HttpStatus.BAD_REQUEST,
-      );
     return this.authService.login(body);
   }
 }
