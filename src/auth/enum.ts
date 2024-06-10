9582224be19c4e30906d66dbb5c68ede8e4014cf
benjabarreiro <barreirobenjamin@gmail.com>
2024-06-10 07:53:07 -0300

    refactor: move enum ts into folder

--- src/auth/enum.ts
+++
@@ -1,7 +0,0 @@
-enum Role {
-  Patient = 1,
-  Secretary = 2,
-  Doctor = 3,
-  Healthcare = 4,
-  Administrator = 5,
-}
---
+++ src/auth/enums/index.ts
@@ -0,0 +1,7 @@
+export enum Roles {
+  Patient = Number(process.env.Patient),
+  Secretary = Number(process.env.Secretary),
+  Doctor = Number(process.env.Doctor),
+  Healthcare = Number(process.env.Healthcare),
+  Administrator = Number(process.env.Administrator),
+}
