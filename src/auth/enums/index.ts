e56e00dfc5fe17ab5b7e14de98559ee0fd989dc5
benjabarreiro <barreirobenjamin@gmail.com>
2024-06-14 08:10:27 -0300

    refactor: move modules into modules folder

--- src/auth/enums/index.ts
+++
@@ -1,7 +0,0 @@
-export enum Roles {
-  Patient = Number(process.env.Patient),
-  Secretary = Number(process.env.Secretary),
-  Doctor = Number(process.env.Doctor),
-  Healthcare = Number(process.env.Healthcare),
-  Administrator = Number(process.env.Administrator),
-}
