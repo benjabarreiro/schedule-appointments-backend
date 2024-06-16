f176261cc3c81863dfecb6828b10e509dee417e7
benjabarreiro <barreirobenjamin@gmail.com>
2024-06-16 16:55:18 -0300

    refactor: remove unused variables

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -6,13 +6,6 @@ import { Connection, Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  private readonly filePath = path.join(__dirname, '..', 'users.json');
-  private readonly filePathSrc = path.join(
-    __dirname,
-    '../../src',
-    'users.json',
-  );
-
   private userRepository: Repository<User>;
   constructor(private readonly connection: Connection) {
     this.userRepository = this.connection.getRepository(User);
