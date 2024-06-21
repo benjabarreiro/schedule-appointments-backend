bcf77f31a0ed7511c0ea90e74c358eb5b60398cf
benjabarreiro <barreirobenjamin@gmail.com>
2024-06-21 09:25:30 -0300

    feat: create updateUser endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Put } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from 'src/auth/dtos';
 
 @Controller('/users')
 export class UsersController {
-  constructor() {}
+  constructor(private readonly usersService: UsersService) {}
+
+  @Put('/:id')
+  async updateUser(@Body('body') body: UpdateUserDto, @Param('id') id: string) {
+    return this.usersService.updateUser(body, Number(id));
+  }
 }
