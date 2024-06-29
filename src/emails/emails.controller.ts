d6da5e24c0616fe698c99176a6e7ff617b7579e8
benjabarreiro <barreirobenjamin@gmail.com>
2024-06-29 08:30:14 -0300

    feat: create post send email endpoint for tests

--- src/emails/emails.controller.ts
+++ src/emails/emails.controller.ts
@@ -1,6 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { EmailsService } from './email.servicie';
 
 @Controller('/emails')
 export class EmailsController {
-  constructor() {}
+  constructor(private emailService: EmailsService) {}
+
+  // create endpoint for tests
+  @Post()
+  async sendEmail(@Body() body) {
+    return await this.emailService.sendEmail(body.to, body.subject, body.text);
+  }
 }
