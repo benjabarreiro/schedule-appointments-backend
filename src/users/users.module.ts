f7dd35c3389f35848c3a95f31be67977bf59d63f
benjabarreiro <barreirobenjamin@gmail.com>
2024-07-06 10:29:29 -0300

    feat: implement new jwt module in auth and users modules

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { JwtModule } from 'src/jwt/jwt.module';
 
 @Module({
+  imports: [JwtModule],
   exports: [UsersService],
   controllers: [UsersController],
   providers: [UsersService],
