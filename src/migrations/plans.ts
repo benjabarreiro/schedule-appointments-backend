c22533c51c3dad2a26e7cbd72d703bd11218d1b4
benjabarreiro <barreirobenjamin@gmail.com>
2024-07-06 11:45:38 -0300

    feat: create plans migration, module and service

---
+++ src/migrations/plans.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Plans1720276915894 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
