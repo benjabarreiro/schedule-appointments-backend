4275fb9b0092b23d46edff309210ab64aaafdb81
benjabarreiro <barreirobenjamin@gmail.com>
2024-07-06 12:01:29 -0300

    feat: create plans migration file

--- src/migrations/plans.ts
+++ src/migrations/plans.ts
@@ -1,11 +1,26 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class Plans1720276915894 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'plans',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          { name: 'name', type: 'varchar' },
+          { name: 'price', type: 'decimal', precision: 10, scale: 2 },
+        ],
+      }),
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('plans');
+  }
 }
