939ac328c043d76d71ff98b13dea4581b7a3a072
benjabarreiro <barreirobenjamin@gmail.com>
2024-07-06 10:30:04 -0300

    feat: create user guard to validate user can do certain action

---
+++ src/guards/users.guard.ts
@@ -0,0 +1,27 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from 'src/jwt/jwt.service';
+
+@Injectable()
+export class UsersGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const token = request.headers.authorization?.split(' ')[1];
+    if (!token) {
+      return false;
+    }
+
+    const decoded = this.jwtService.verifyToken(token);
+
+    const userIdFromJWT = decoded['id'];
+
+    const userIdFromParams = request.params.id;
+
+    return userIdFromJWT === userIdFromParams;
+  }
+}
