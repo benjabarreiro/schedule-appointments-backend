d5c96f2cbb8b187abd357bdbfe74eaa2e4639836
benjabarreiro <barreirobenjamin@gmail.com>
2024-07-07 09:44:16 -0300

    feat: create business controller with create and add user endpoints

---
+++ src/businesses/businesses.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { BusinessesService } from './businesses.service';
+
+@Controller('businesses')
+export class BusinessesController {
+  constructor(private readonly businessesService: BusinessesService) {}
+  @Post('/create')
+  async createBusiness(@Body() body: any) {
+    try {
+      return await this.businessesService.createBusiness(body);
+    } catch (err) {}
+  }
+
+  @Post('/add-user')
+  async addUserToBusiness(@Body() body) {
+    try {
+      return await this.businessesService.addUserToBusiness(
+        body.userId,
+        body.businessId,
+      );
+    } catch (err) {}
+  }
+}
