462ae6ca602e289ac3e6b42d9029300063f61624
benjabarreiro <barreirobenjamin@gmail.com>
2024-07-10 15:07:49 -0300

    refactor: add relation with business and schedules for employee

--- src/employees/entities/employee.entity.ts
+++ src/employees/entities/employee.entity.ts
@@ -2,10 +2,16 @@ import {
   Column,
   Entity,
   JoinColumn,
+  JoinTable,
+  ManyToMany,
+  OneToMany,
   OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { User } from '../../users/entities/user.entity';
+import { Schedule } from '../../schedules/schedule.entity';
+import { Business } from '../../businesses/business.entity';
+import { Exclude } from 'class-transformer';
 
 @Entity('employees')
 export class Employee {
@@ -18,4 +24,16 @@ export class Employee {
   @OneToOne(() => User)
   @JoinColumn({ name: 'user_id' })
   user: User;
+
+  @ManyToMany(() => Business, (business) => business.employees)
+  @JoinTable({
+    name: 'employee_business', // name of the junction table
+    joinColumn: { name: 'employee_id', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'business_id', referencedColumnName: 'id' },
+  })
+  @Exclude()
+  businesses: Business[];
+
+  @OneToMany(() => Schedule, (schedule) => schedule.business)
+  schedules: Schedule[];
 }
