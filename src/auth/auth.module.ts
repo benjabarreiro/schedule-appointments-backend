3a8f80c30c4db9a179ed174ac91303b1c0a0b442
benjabarreiro <barreirobenjamin@gmail.com>
2024-07-19 07:48:29 -0300

    feat: add more values to jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,17 @@ import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { EmailsModule } from 'src/emails/emails.module';
 import { JwtModule } from 'src/jwt/jwt.module';
+import { BusinessesModule } from 'src/businesses/businesses.module';
+import { EmployeesModule } from 'src/employees/employees.module';
 
 @Module({
-  imports: [UsersModule, EmailsModule, JwtModule],
+  imports: [
+    UsersModule,
+    EmailsModule,
+    JwtModule,
+    BusinessesModule,
+    EmployeesModule,
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
