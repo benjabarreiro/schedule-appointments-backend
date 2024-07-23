f828e67bd5bb27f0c9dc2a150f6d3488f77504cf
benjabarreiro <barreirobenjamin@gmail.com>
2024-07-23 07:39:01 -0300

    fix: add jwt module to imports in business and schedule module

--- src/schedules/schedules.module.ts
+++ src/schedules/schedules.module.ts
@@ -3,9 +3,10 @@ import { SchedulesService } from './schedules.service';
 import { EmployeesModule } from 'src/employees/employees.module';
 import { BusinessesModule } from 'src/businesses/businesses.module';
 import { SchedulesController } from './schedules.controller';
+import { JwtModule } from 'src/jwt/jwt.module';
 
 @Module({
-  imports: [EmployeesModule, BusinessesModule],
+  imports: [EmployeesModule, BusinessesModule, JwtModule],
   exports: [SchedulesService],
   providers: [SchedulesService],
   controllers: [SchedulesController],
