0b818afe69218cb0317cc8f89aac9ec3f542e8f8
benjabarreiro <barreirobenjamin@gmail.com>
2024-07-26 08:24:51 -0300

    fix: import jwt module in modules

--- src/businesses/businesses.module.ts
+++ src/businesses/businesses.module.ts
@@ -3,9 +3,10 @@ import { UsersModule } from 'src/users/users.module';
 import { BusinessesService } from './businesses.service';
 import { BusinessesController } from './businesses.controller';
 import { EmployeesModule } from 'src/employees/employees.module';
+import { JwtModule } from 'src/jwt/jwt.module';
 
 @Module({
-  imports: [UsersModule, EmployeesModule],
+  imports: [UsersModule, EmployeesModule, JwtModule],
   controllers: [BusinessesController],
   providers: [BusinessesService],
   exports: [BusinessesService],
