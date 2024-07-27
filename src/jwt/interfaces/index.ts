da231f935c4d74ab465f368025227f7d8a186a93
benjabarreiro <barreirobenjamin@gmail.com>
2024-07-27 08:34:46 -0300

    refactor: add additional types to IJwt

--- src/jwt/interfaces/index.ts
+++ src/jwt/interfaces/index.ts
@@ -3,4 +3,7 @@ import { JwtPayload } from 'jsonwebtoken';
 export interface IJwt extends JwtPayload {
   email: string;
   id: number;
+  roleId: number;
+  businessId: number;
+  employeeId: number;
 }
