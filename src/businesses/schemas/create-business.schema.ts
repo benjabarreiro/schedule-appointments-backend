d6d70315259e8cd0305c7b330f7a54c2f4864974
benjabarreiro <barreirobenjamin@gmail.com>
2024-08-11 12:55:00 -0300

    fix: validate that booleans and numbers come in correct type

--- src/businesses/schemas/create-business.schema.ts
+++ src/businesses/schemas/create-business.schema.ts
@@ -2,6 +2,24 @@ import * as Joi from 'joi';
 
 export const createBusinessSchema = Joi.object({
   name: Joi.string().required().not().empty().min(10),
-  plan: Joi.number().required().not().empty(),
-  isActive: Joi.boolean().required().not().empty(),
+  plan: Joi.number()
+    .required()
+    .not()
+    .empty()
+    .custom((value, helper) => {
+      if (typeof helper.original === 'string') {
+        throw Error('Plan must be a number');
+      }
+      return value;
+    }),
+  isActive: Joi.boolean()
+    .required()
+    .not()
+    .empty()
+    .custom((value, helper) => {
+      if (typeof helper.original === 'string') {
+        throw Error('isActive must be a boolean');
+      }
+      return value;
+    }),
 });
