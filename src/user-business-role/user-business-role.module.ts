6678a568bce02a3a48844bf1e4c8013fadffe5de
benjabarreiro <barreirobenjamin@gmail.com>
2024-08-24 14:55:46 -0300

    fix: add user business role service as provider of module

--- src/user-business-role/user-business-role.module.ts
+++ src/user-business-role/user-business-role.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { UserBusinessRoleService } from './user-business-role.service';
 
 @Module({
+  providers: [UserBusinessRoleService],
   exports: [UserBusinessRoleService],
 })
 export class UserBusinessRoleModule {}
