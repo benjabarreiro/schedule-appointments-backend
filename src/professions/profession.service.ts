6afbdd49339ce248d4c14911f6255809a6c02ac1
benjabarreiro <barreirobenjamin@gmail.com>
2024-09-15 11:56:38 -0300

    feat: create profession service

---
+++ src/professions/profession.service.ts
@@ -0,0 +1,53 @@
+import { Connection, Repository } from 'typeorm';
+import { Profession } from './profession.entity';
+
+export class ProfessionsService {
+  private professionsRepository: Repository<Profession>;
+
+  constructor(private readonly connection: Connection) {
+    this.professionsRepository = connection.getRepository(Profession);
+  }
+
+  async getProfession(id: number) {
+    try {
+      return await this.professionsRepository.findOne({ where: { id } });
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async getAllProfessions() {
+    try {
+      return await this.professionsRepository.find();
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async createProfession(body) {
+    try {
+      const newProfession = this.professionsRepository.create({
+        name: body.name,
+      });
+      return await this.professionsRepository.save(newProfession);
+    } catch (err) {
+      throw err;
+    }
+  }
+  async editProfession(id, body) {
+    try {
+      const profession = await this.getProfession(id);
+      const editedProfessionBody = { ...profession, name: body.name };
+      return await this.professionsRepository.save(editedProfessionBody);
+    } catch (err) {
+      throw err;
+    }
+  }
+  async deleteProfession(id: number) {
+    try {
+      return await this.professionsRepository.delete(id);
+    } catch (err) {
+      throw err;
+    }
+  }
+}
