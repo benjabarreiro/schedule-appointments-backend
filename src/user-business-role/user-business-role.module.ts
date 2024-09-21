fe50362b67fc3d6906cf6382f31650face648476
benjabarreiro <barreirobenjamin@gmail.com>
2024-09-21 12:57:24 -0300

    feat: import user business role controller and employess profession module

--- src/user-business-role/user-business-role.module.ts
+++ src/user-business-role/user-business-role.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserBusinessRoleService } from './user-business-role.service';
+import { UserBusinessRoleController } from './user-business-role.controller';
+import { EmployeesProfessionModule } from 'src/employees-professions/employees-profession.module';
 
 @Module({
   providers: [UserBusinessRoleService],
   exports: [UserBusinessRoleService],
+  controllers: [UserBusinessRoleController],
+  imports: [EmployeesProfessionModule],
 })
 export class UserBusinessRoleModule {}
