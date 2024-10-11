2bb26205e7a8ba7e6078c830f1f460179e832dff
benjabarreiro <barreirobenjamin@gmail.com>
2024-10-11 11:39:21 -0300

    feat: create CRUD endpoints and the services needed for them to work

--- src/appointments/appointments.controller.ts
+++ src/appointments/appointments.controller.ts
@@ -1,1 +1,42 @@
-export class AppointmentsController {}
+import { Body, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { AppointmentsService } from './appointments.service';
+
+export class AppointmentsController {
+  constructor(private readonly appointmentsService: AppointmentsService) {}
+
+  @Post()
+  async createAppointment(body) {
+    try {
+      return await this.appointmentsService.createAppointment(body);
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  @Put(':id')
+  async updateAppointment(@Param('id') id, @Body() body) {
+    try {
+      return await this.appointmentsService.updateAppointment(id, body);
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  @Delete(':id')
+  async cancelAppointment(@Param('id') id) {
+    try {
+      return await this.appointmentsService.cancelAppointment(id);
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  @Get('id')
+  async getAppointmentById(@Param('id') id) {
+    try {
+      return await this.appointmentsService.getAppointmentByIdOrThrow(id);
+    } catch (err) {
+      throw err;
+    }
+  }
+}
--- src/appointments/appointments.service.ts
+++ src/appointments/appointments.service.ts
@@ -1,1 +1,97 @@
-export class AppointmentsService {}
+import { Connection, Repository } from 'typeorm';
+import { Appointment } from './appointment.entity';
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export class AppointmentsService {
+  private appointmentsRepository: Repository<Appointment>;
+  constructor(private readonly connection: Connection) {
+    this.appointmentsRepository = connection.getRepository(Appointment);
+  }
+
+  async createAppointment(body) {
+    try {
+      await this.isAppointmentAlreadyTaken(body.scheduleId, body.dateTime);
+
+      const newAppointment = await this.appointmentsRepository.create(body);
+
+      return await this.appointmentsRepository.save(newAppointment);
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async updateAppointment(id, body) {
+    try {
+      const appointment = await this.getAppointmentByIdOrThrow(id);
+
+      await this.isAppointmentAlreadyTaken(body.scheduleId, body.dateTime);
+
+      const updatedAppointment = { ...appointment, body };
+
+      return await this.appointmentsRepository.save(updatedAppointment);
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async cancelAppointment(id: number) {
+    try {
+      await this.getAppointmentByIdOrThrow(id);
+
+      return this.appointmentsRepository.delete(id);
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async getAppointmentById(id: number) {
+    try {
+      return await this.appointmentsRepository.findOne({ where: { id } });
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async getAppointmentByDateTime(scheduleId, dateTime) {
+    try {
+      return await this.appointmentsRepository.findOne({
+        where: { schedule: { id: scheduleId }, dateTime },
+      });
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async getAppointmentByIdOrThrow(id) {
+    try {
+      const appointment = await this.getAppointmentById(id);
+      if (!appointment)
+        throw new HttpException(
+          'There are no appointments with id ' + id,
+          HttpStatus.NOT_FOUND,
+        );
+      return appointment;
+    } catch (err) {
+      throw err;
+    }
+  }
+  async isAppointmentAlreadyTaken(scheduleId, dateTime) {
+    try {
+      const isAlreadyTaken = await this.getAppointmentByDateTime(
+        scheduleId,
+        dateTime,
+      );
+
+      if (isAlreadyTaken) {
+        throw new HttpException(
+          'Appointment already exists for dateTime',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+
+      return;
+    } catch (err) {
+      throw err;
+    }
+  }
+}
